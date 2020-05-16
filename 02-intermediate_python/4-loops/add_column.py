1102ee2a956159272fc2b96dd7b1efb582a98e7b
Mitch Israel <48568215+mitchisrael88@users.noreply.github.com>
2020-05-16 04:29:05 -0400

    Create add_column.py

---
+++ 02-intermediate_python/4-loops/add_column.py
@@ -0,0 +1,11 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+print(cars)
+
+# Code for loop that adds COUNTRY column
+for lab, row, in cars.iterrows():
+    cars.loc[lab, "COUNTRY"] = str.upper(row["country"])
+
+# Print cars
+print(cars)
