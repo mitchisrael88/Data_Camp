261996fbd8f4dccfa4ea311b27a008a2d66a91e9
Mitch Israel <48568215+mitchisrael88@users.noreply.github.com>
2020-06-01 04:29:45 -0400

    Add files via upload

---
+++ 03-data_manipulation_with_pandas/01-transforming_data/inspecting_a_dataframe.py
@@ -0,0 +1,13 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
---
+++ 03-data_manipulation_with_pandas/01-transforming_data/subsetting_columns1.py
@@ -0,0 +1,11 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+KeyboardInterrupt
+>>> 
+=============================== RESTART: Shell ===============================
+>>> # Select the individuals column
+individuals = homelessness["individuals"]
+
+# Print the head of the result
+print(individuals.head())
