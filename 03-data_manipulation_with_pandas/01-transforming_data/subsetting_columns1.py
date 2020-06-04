c58f31bb95f61a6c5eda5ab6afadb9f2f18f2ab5
Mitch Israel <48568215+mitchisrael88@users.noreply.github.com>
2020-06-04 00:03:23 -0400

    Update subsetting_columns1.py

--- 03-data_manipulation_with_pandas/01-transforming_data/subsetting_columns1.py
+++ 03-data_manipulation_with_pandas/01-transforming_data/subsetting_columns1.py
@@ -1,10 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-KeyboardInterrupt
->>> 
-=============================== RESTART: Shell ===============================
->>> # Select the individuals column
+# Select the individuals column
 individuals = homelessness["individuals"]
 
 # Print the head of the result
