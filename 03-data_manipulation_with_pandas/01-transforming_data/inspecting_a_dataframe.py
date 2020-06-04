56d161d8fa4a277e43b5d5d61f586b22ce08d2f6
Mitch Israel <48568215+mitchisrael88@users.noreply.github.com>
2020-06-03 23:58:07 -0400

    Update inspecting_a_dataframe.py

--- 03-data_manipulation_with_pandas/01-transforming_data/inspecting_a_dataframe.py
+++ 03-data_manipulation_with_pandas/01-transforming_data/inspecting_a_dataframe.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # Print the head of the homelessness data
+# Print the head of the homelessness data
 print(homelessness.head())
 
 # Print information about homelessness
