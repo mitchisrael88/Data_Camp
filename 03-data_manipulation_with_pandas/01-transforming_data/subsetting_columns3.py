134a827a07bf634c2b3a8cd65b77fbd78cce2744
Mitch Israel <48568215+mitchisrael88@users.noreply.github.com>
2020-06-04 00:03:53 -0400

    Update subsetting_columns3.py

--- 03-data_manipulation_with_pandas/01-transforming_data/subsetting_columns3.py
+++ 03-data_manipulation_with_pandas/01-transforming_data/subsetting_columns3.py
@@ -1,10 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-KeyboardInterrupt
->>> 
-=============================== RESTART: Shell ===============================
->>> # Select only the individuals and state columns, in that order
+# Select only the individuals and state columns, in that order
 ind_state = homelessness[["individuals", "state"]]
 
 # Print the head of the result
