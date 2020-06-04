fd000ba3586cdc04091aebdc213c8f8d0f9ef014
Mitch Israel <48568215+mitchisrael88@users.noreply.github.com>
2020-06-04 00:03:03 -0400

    Update sorting_rows3.py

--- 03-data_manipulation_with_pandas/01-transforming_data/sorting_rows3.py
+++ 03-data_manipulation_with_pandas/01-transforming_data/sorting_rows3.py
@@ -1,8 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-KeyboardInterrupt
->>> # Sort homelessness by region, then descending family members
+# Sort homelessness by region, then descending family members
 homelessness_reg_fam = homelessness.sort_values(["region","family_members"], ascending=[True, False])
 
 # Print the top few rows
